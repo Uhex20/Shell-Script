804b099816d7bca8a3ebe4b7edd40ceec22d2fbf
Uhex20 <64974663+Uhex20@users.noreply.github.com>
2020-05-07 18:43:06 -0400

    Create rename_renumber.sh
    This script allows you to rename the files in the same directory that located. The scrip only run by command line.  
    
    
    USAGE:
     ...$  sh rename_renumber.sh filename ExtensionName
    
    Output Example:
    filename001.ExtensionName
    filename002.ExtensionName
    filename003.ExtensionName
    ...

---
+++ rename_renumber.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+baseName=$1
+fileExtension=$2
+
+counter=1
+for i in *.*; do
+   renameArg=$(printf "$baseName%03d.$fileExtension" "$counter") #03 pad to length of 3
+    mv -i -- "$i" "$renameArg"
+    counter=$((counter+1))
+done
